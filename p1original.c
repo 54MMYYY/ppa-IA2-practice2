03ba9e90207b2ac5b95cc9d12fddac95bd274bea
Samson <95264901+54MMYYY@users.noreply.github.com>
2022-03-29 16:24:46 +0000

    p2

--- p1original.c
+++ p1original.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-void input(float *a, float *b)
-{
-  printf("Enter the base and height of the triangle\n");
-  scanf("%f %f",a,b);
+void input(float *a, float *b){
+  printf("Enter the base and height length\n");
+  scanf("%f%f",a,b);
 }
-void find_area(float a , float b, float *c)
-{
+void find_area(float a , float b, float *c){
   *c=((a*b)/2);
 }
-void output(float a, float b, float c)
-{
-  printf("The area of the traingle with base %0.3f and height %0.3f is %0.3f", a, b, c);
+  
+void output(float a, float b, float c){
+  printf("The area of the tringle with base %0.3f and height %0.3f is %0.3f",a,b,c);
 }
-int main()
-{
+
+int main(){
   float a,b,c;
   input(&a,&b);
   find_area(a,b,&c);
--- p2original.c
+++ p2original.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int input_side(){
-  int a,b,c;
-  printf("Enter the length of a side: ");
+  int a;
+  printf("Enter the length of the sides of the triangle\n");
   scanf("%d",&a);
+  return a;
 }
 
 int check_scalene(int a, int b, int c){
   int isscalene;
-  if(a!=b && a!=c && b!=c){
+  if(a!=b && b!=c && a!=c){
     isscalene=0;
   }
   return isscalene;
@@ -16,9 +17,11 @@ int check_scalene(int a, int b, int c){
 
 void output(int a, int b, int c, int isscalene){
   if(isscalene==0){
-    printf("The triangle iss scalene for lengths %d,%d,%d",a,b,c);
+    printf("The triangle with sides %d %d and %d is scalene",a,b,c);
   }
-  printf("The triangle is not scalene for lengths %d,%d,%d",a,b,c);
+  else{
+  printf("The triangle with sides %d %d and %d is not scalene",a,b,c);
+    }
 }
 
 int main(){
